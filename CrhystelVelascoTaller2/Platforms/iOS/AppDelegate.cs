784cd8feb8acfe5862abf5a98d0a045eb6698a13
Crhystel <crhystelvelasco213@gmail.com>
2024-12-01 15:50:44 -0500

    Agregar archivos de proyecto.

---
+++ CrhystelVelascoTaller2/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CrhystelVelascoTaller2
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
